997144a806762f0cf44f40be43c912d2872e5b1e
lianhuo <1848464395@qq.com>
2017-12-12 14:04:00 +0800

    初始化

--- net/socketUDPserver.py
+++ net/socketUDPserver.py
@@ -14,7 +14,7 @@ try:
     print('Bind UDP to 31323')
     while True:
         data, addr = s.recvfrom(1024)
-        print('Received from %s:%s.' % addr)
+        print('Received from %s:%s.:%s' % (addr, data))
         s.sendto(b'Hello, %s!' % data, addr)
     pass
 except IOError as e:
